e5e4d2f4a33bdb2fc762ae821a877769540e71f2
unknown <dumbirichuks@gmail.com>
2023-10-13 12:44:20 +0100

    building car rentals website with nextjs and typescript

---
+++ components/CarCard.tsx
@@ -0,0 +1,47 @@
+import { CarProps } from '@/types'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+    car: CarProps
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+    const carRent = calculateCarRent(city_mpg, year);
+    return (
+        <div className="car-card group">
+            <div className="car-card__content">
+                <h2 className='car-card__content-title'>
+                    {make}{model}
+                </h2>
+            </div>
+
+            <p className='flex mt-6 text-[31px] font-extrabold items-center'>
+                <span className="self-start text-[14px] font-semibold">
+                    $
+                </span>
+                {carRent}
+                <span className="self-end text-[14px] font-semibold">
+                    /day
+                </span>
+            </p>
+
+            <div className="relative w-full h-40 my-3 object-contain">
+                <Image
+                    src="/hero.png"
+                    alt="Car model"
+                    fill
+                    priority
+                    className='object-contain'
+                />
+            </div>
+
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default CarCard
